4e4784fabf5e25ed5449039feef332c14c454d84
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2025-01-30 23:12:05 +0530

    Searching the dramas and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
+import search from "./components/Search.jsx";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -16,18 +19,50 @@ const API_OPTIONS = {
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query = '') => {
+        setIsLoading(true);
+        setErrorMessage('')
         try {
+            //const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            //const endpoint = `${API_BASE_URL}/discover/tv?sort_by=popularity.desc&with_original_language=ko&with_genres=18&api_key=${API_KEY}`;
+            const endpoint = query
+                ? `${API_BASE_URL}/search/tv?query=${encodeURIComponent(query)}&api_key=${API_KEY}`
+                : `${API_BASE_URL}/discover/tv?sort_by=popularity.desc&with_original_language=ko&with_genres=18&first_air_date.gte=2020-01-01&api_key=${API_KEY}`;
+
+            const response = await fetch(endpoint, API_OPTIONS);
+            if(!response.ok) {
+                throw new Error('FAiled to fetch movies');
+            }
+
+            const data = await response.json();
+            // if(data.Response === 'False'){
+            //     setErrorMessage(data.Error || 'Failed to fetch dramas')
+            //     setMovieList([]);
+            //     return;
+            // }
+            // setMovieList(data.results || []);
+            if (data.results) {
+                // 🔹 Filter only Korean dramas
+                const filteredResults = data.results.filter(movie => movie.original_language === 'ko');
+                setMovieList(filteredResults);
+            } else {
+                setMovieList([]);
+                setErrorMessage('No results found.');
+            }
 
         }catch (error) {
             console.log(`Error fetching movies : ${error}`);
             setErrorMessage(`Error fetching movies. Please try again later.`)
+        }finally {
+            setIsLoading(false);
         }
     }
     useEffect(() => {
-
-    }, []);
+            fetchMovies(searchTerm);
+    }, [searchTerm]);
     return (
         <main>
             <div className="pattern"/>
@@ -41,7 +76,20 @@ const App = () => {
                 </header>
 
                <section className="all-movies">
-                   <h2>All Movies</h2>
+                   <h2 className="mt-[40px]">All KDramas</h2>
+
+                   {isLoading ? (
+                       <Spinner/>
+                   ) : errorMessage ? (
+                       <p className="text-red-500">{errorMessage}</p>
+                   ) : (
+                       <ul>
+                           {movieList.map((movie) => (
+                               <MovieCard key={movie.id} movie={movie}/>
+                           ))}
+                       </ul>
+                   )}
+
                </section>
             </div>
         </main>
